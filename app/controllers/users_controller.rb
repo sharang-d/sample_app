1f24e41d3717c6f42b5588e62866add641d1506c
Sharang Dashputre <sharang.dashputre@weboniselab.com>
2014-01-08 23:48:31 +0530

    sign in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -21,6 +22,7 @@ class UsersController < ApplicationController
   def index
   end
 
+  
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
