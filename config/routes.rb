9edf7c44d22200714589be6e30cd731155758fb5
Sharang Dashputre <sharang.dashputre@weboniselab.com>
2014-01-09 13:55:02 +0530

    Follow functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 SampleApp::Application.routes.draw do
   root 'static_pages#home'
-  resources :sessions, only: [:new, :create, :destroy]
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
-  resources :microposts, only: [:create, :destroy]
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
